2f6336b9303493d3892ea7e8f4ed8a6f6fad44de
oref <orefdev@gmail.com>
2019-07-23 17:06:33 +0100

    added flatpages

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
+
+DEBUG=True
+FLATPAGES_AUTO_RELOAD=DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/<path:path>/')
+def page(path):
+    return pages.get_or_404(path).html
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8081)
---
+++ pages/hello-world.md
@@ -0,0 +1,4 @@
+title: Hello World
+date: 2019-07-23
+
+** Hello World, from a *page*!

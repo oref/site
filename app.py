2721451cfba505695838f995103d7d4b723510ed
oref <orefdev@gmail.com>
2019-07-25 12:57:30 +0100

    added blog page

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 import sys
-
+import webbrowser
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
-from flask_frozen import Freezer
 
-DEBUG = True
-FLATPAGES_AUTO_RELOAD = DEBUG
 FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
-freezer = Freezer(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html', pages=pages)
+def home():
+    return render_template('base.html')
 
-@app.route('/tag/<string:tag>/')
-def tag(tag):
-    tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-    return render_template('tag.html', pages=tagged, tag=tag)
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', pages=pages)
 
 @app.route('/<path:path>/')
 def page(path):
     page = pages.get_or_404(path)
     return render_template('page.html', page=page)
 
+@app.route('/projects')
+def projects():
+    pass
+
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == "build":
-        freezer.freeze()
-    else:
-        app.run(port=8000)
+    app.run(debug=True, host='0.0.0.0')

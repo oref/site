2cd9ab872013cf17a29d836bc1b335ef4c00fb27
oref <orefdev@gmail.com>
2019-07-26 14:07:25 +0100

    some styling

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        app.run(host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0')
